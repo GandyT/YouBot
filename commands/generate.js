b949e79beb9060bc7acf40a34f223feb9a98cfa1
BuildTools <randysim9@gmail.com>
2021-04-13 13:07:46 -0400

    lazy fixes

--- commands/generate.js
+++ commands/generate.js
@@ -14,10 +14,14 @@ module.exports = {
         const { message, args, client } = env; // Variables
         if (!Data.userExists(message.author.id)) return message.channel.send("You do not have any data");
         var text = args.slice(1);
+        if (!text.length) return message.channel.send("You have to say something")
 
         for (let i = 0; i < 30; ++i) {
             var generated = Data.nextWord(message.author.id, text[text.length - 1]);
             if (generated == text[text.length - 1]) break;
+            if (generated == text[text.length - 2]) break;
+            if (generated == text[text.length - 3]) break;
+            if (generated == text[text.length - 4]) break;
             text.push(generated);
         }
 
